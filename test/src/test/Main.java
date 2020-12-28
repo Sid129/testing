b776f3458dab865245ec4b0c1daf2e476f71a477
Siddalingappa Uppar <siddalingappa.uppar@sap.com>
2020-12-28 17:22:45 +0530

    adding system variable

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -5,6 +5,8 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Perfect");
+      String value = System.getenv("Name");
+      System.out.println(value);
 	}
 
 }
