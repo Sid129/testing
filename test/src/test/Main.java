6a712e2ebc619aee915ddba22481e986b13e755e
Siddalingappa Uppar <siddalingappa.uppar@sap.com>
2021-01-04 08:29:32 +0530

    Adding Properties file

--- test/src/test/Main.java
+++ test/src/test/Main.java
@@ -1,14 +1,24 @@
 package test;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
       System.out.println("Perfect");
       System.out.println("yes");
       String value = System.getenv("NAME");
       System.out.println("yess");
       System.out.println("variable= "+value);
+      FileInputStream config= new FileInputStream("./config.properties");
+  	 Properties prop=new Properties();
+  	 prop.load(config);	
+     String url=prop.getProperty("Name");
+     System.out.println("Plugin"+url);
 	}
 
 }
